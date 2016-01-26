24f605003e90f97b8c12e82487d4750f8a4e03a4
Waisercat <waisercat@gmail.com>
2016-01-26 17:52:00 +0100

    Cambio del index (vista)
    Pagina index del directorio raiz

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-?>
-
-Hola mundo
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>{titulo}</title>
+        <!--<meta name="title" content={Título de la Página}-->
+        <!--<meta name="keywords" content="{keywords}">-->
+        <!--<meta name="description" content="{descripción}">-->
+        <link type="text/css" rel="stylesheet" href="css/template.css">
+    </head>
+    <body>
+        <div id="page">
+            <div id="cabecera">
+                <img alt="home page" src="img/logo.gif"></a>
+            </div>
+            <div id="contenido">
+                <h1>{titulo}</h1>
+            </div>
+        </div>
+    </body>
+</html>
